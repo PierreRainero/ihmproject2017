7cf908a01eac87553e695ad79001b30afbd74e51
Simon Serrano <simon.serrano@etu.unice.fr>
2017-02-13 20:14:14 +0100

    Ajout de deux vues pour les enseignes : - la vue pour tous les produits                                                                       - la vue pour zoomer sur un produit en particulier
    Modification pour la présentation de demain des logos et des images des produits, ainsi que le contenu de certains labels

--- firm/src/main/java/polytech/unice/si3/ihm/b/MainApp.java
+++ firm/src/main/java/polytech/unice/si3/ihm/b/MainApp.java
@@ -18,18 +18,19 @@ public class MainApp extends Application {
 
     public void start(Stage stage) throws Exception {
 
-        log.info("Starting Hello JavaFX and Maven demonstration application");
+        log.info("Starting firm view");
 
         String fxmlFile = "/fxml/hello.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
+        stage.setMinHeight(600.0);
+        stage.setMinWidth(400.0);
 
         log.debug("Showing JFX scene");
-        Scene scene = new Scene(rootNode, 400, 200);
+        Scene scene = new Scene(rootNode, 1280, 720);
         scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Hello JavaFX and Maven");
+        stage.setTitle("Enseigne du centre commercial");
         stage.setScene(scene);
         stage.show();
     }
