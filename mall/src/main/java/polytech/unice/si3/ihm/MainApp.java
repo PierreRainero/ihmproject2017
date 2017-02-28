7c0cb3f03ccc12ed56efae0efbaabb7fbe07998a
Ken Roulamellah <ken.roulamellah@etu.unice.fr>
2017-03-01 00:09:18 +0100

    Ajout du mainViewController

--- mall/src/main/java/polytech/unice/si3/ihm/MainApp.java
+++ mall/src/main/java/polytech/unice/si3/ihm/MainApp.java
@@ -7,19 +7,22 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import polytech.unice.si3.ihm.view.MainViewController;
+
+import java.io.IOException;
 
 public class MainApp extends Application {
 
     private static final Logger log = LoggerFactory.getLogger(MainApp.class);
+    private Stage stage;
 
     public static void main(String[] args) throws Exception {
         launch(args);
     }
 
     public void start(Stage stage) throws Exception {
-
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
+        this.stage = stage;
+        log.info("Starting main");
         String fxmlFile = "/fxml/main.fxml";
         log.debug("Loading FXML for main view from: {}", fxmlFile);
         FXMLLoader loader = new FXMLLoader();
@@ -27,9 +30,28 @@ public class MainApp extends Application {
 
         log.debug("Showing JFX scene");
         Scene scene = new Scene(rootNode, 1280, 720);
-        scene.getStylesheets().add("/styles/styles.css");
+        //scene.getStylesheets().add("/styles/styles.css");
 
         stage.setTitle("Centre Commercial");
+        stage.setScene(scene);
+
+        MainViewController controller = loader.getController();
+        controller.setMainApp(this);
+
+        stage.show();
+
+    }
+
+    public void changeScene(String fxml){
+        String fxmlFile = fxml;
+        FXMLLoader loader = new FXMLLoader();
+        Parent node = null;
+        try {
+            node = loader.load(getClass().getResourceAsStream(fxmlFile));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(node, 1280, 720);
         stage.setScene(scene);
         stage.show();
     }
