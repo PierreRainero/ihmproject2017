f29d34bde05811174f2391048b33e96effe22aa6
Fabien Durando <fabien.durando@etu.unice.fr>
2017-02-28 15:26:17 +0100

    Ajout Product et Promotion

---
+++ shop/src/main/java/polytech/unice/si3/ihm/shop/model/Product.java
@@ -0,0 +1,71 @@
+package polytech.unice.si3.ihm.shop.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ *
+ */
+public class Product {
+    private String name;
+    private String imageURL;
+    private double price;
+    private Optional<Promotion> promotion;
+    private List<String> objectTypes;
+
+    /**
+     * Constructeur normal de produit
+     * @param name nom du produit
+     * @param imageURL url de l'image correspondant au produit
+     * @param price prix du produit
+     */
+    public Product(String name, String imageURL, double price){
+        this.name = name;
+        this.imageURL = imageURL;
+        this.price = price;
+        this.promotion = Optional.empty();
+        this.objectTypes = new ArrayList<String>();
+    }
+
+    /**
+     * Retourne le prix de base de l'objet.
+     * @return price
+     */
+    public double getPrice(){
+        return this.price;
+    }
+
+    /**
+     * Retourne le prix avec promotion de l'objet
+     * @return prix de l'objet avec promotion, -1 si il n'est pas en promotion
+     */
+    public double getPromotedPrice(){
+        if(this.promotion.isPresent()){
+            return this.price * promotion.get().getPercent();
+        }else {
+            return -1;
+        }
+    }
+
+    public void setPromotion(Promotion promotion){
+        this.promotion = Optional.of(promotion);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getImageURL() {
+        return this.imageURL;
+    }
+
+    public void addObjectType(String s){
+        objectTypes.add(s);
+    }
+
+    public List<String> getObjectTypes(){
+        return this.objectTypes;
+    }
+}
