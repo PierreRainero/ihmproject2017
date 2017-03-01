6cf4fa7cd07dee53ca9b5f68453d7bd80b883ed0
Simon Serrano <simon.serrano@etu.unice.fr>
2017-03-01 09:15:46 +0100

    La listView de tous les magasins fonctionnes

--- firm/src/main/java/polytech/unice/si3/ihm/firm/controller/AllStoreController.java
+++ firm/src/main/java/polytech/unice/si3/ihm/firm/controller/AllStoreController.java
@@ -7,10 +7,13 @@ import javafx.fxml.FXML;
 import javafx.geometry.Orientation;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
 import polytech.unice.si3.ihm.firm.model.Firm;
 import polytech.unice.si3.ihm.firm.model.Store;
 
@@ -29,51 +32,61 @@ public class AllStoreController extends BasicController {
     private Button exit;
 
     @FXML
-    private ListView<HBox> stores = new ListView<HBox>();
+    private ListView<Store> stores;
 
 
     /**
      * Method permitting to change the content of the listView
      */
     public void changeListView(){
+        ObservableList<Store> storesInListView = makeObservableList();
+        stores.setPlaceholder(new Label("List is empty"));
         stores.setOrientation(Orientation.VERTICAL);
-        ObservableList<HBox> storesInListView = FXCollections.observableArrayList();
-        List<HBox> hBoxesOfStores = makeHboxOfStores();
-        for (HBox hbox : hBoxesOfStores){
-            storesInListView.add(hbox);
-        }
         stores.setItems(storesInListView);
+        stores.setCellFactory(new Callback<ListView<Store>, ListCell<Store>>() {
+            public ListCell<Store> call(ListView<Store> param) {
+                return new ListCell<Store>(){
+
+                    @Override
+                    protected void updateItem(Store store, boolean empty){
+                        super.updateItem(store, empty);
+                        if (!empty){
+                            VBox vbox = new VBox(new Label(store.getName()),
+                                    new Label(store.getDescription()),
+                                    new Label(store.getMallName()),
+                                    new Label(store.getAddress()));
+                            HBox hBox;
+                            if (store.getImage()==null){
+                                hBox = new HBox(new Label("No image"), vbox);
+                            }
+                            else{
+                                ImageView storeImage = new ImageView(store.getImage());
+                                storeImage.setPreserveRatio(true);
+                                storeImage.setFitHeight(50);
+                                storeImage.setFitWidth(50);
+                                hBox = new HBox(storeImage, vbox);
+                            }
+                            hBox.setSpacing(30.0);
+                            setGraphic(hBox);
+                        }
+                    }
+                };
+            }
+        });
+
     }
 
+
     /**
-     * Allow to create a list of HBox from the stores
-     * @return the list of hboxes made from the stores
+     * Method allowing to make an observable list of stores
+     * @return the observable list of stores
      */
-    public List<HBox> makeHboxOfStores(){
-        List<HBox> listViewItems = new ArrayList<HBox>();
-        HBox hbox = new HBox();
-        Label storeName;
-        Label storeDescription;
-        Label storeAddress;
-        Label storeMallName;
-        ImageView storeImage;
+    public ObservableList<Store> makeObservableList(){
+        ObservableList<Store> stores = FXCollections.observableArrayList();
         for (Store store : firm.getStores()){
-            storeName = new Label(store.getName());
-            storeDescription = new Label(store.getDescription());
-            storeMallName = new Label(store.getMallName());
-            storeAddress = new Label(store.getAddress());
-
-            if (store.getImage()==null){
-                hbox.getChildren().addAll(storeName, storeMallName, storeAddress, storeDescription);
-            }
-            else {
-                storeImage = new ImageView(store.getImage());
-                hbox.getChildren().addAll(storeImage, storeName, storeMallName, storeAddress, storeDescription);
-            }
-
-            listViewItems.add(hbox);
+            stores.addAll(store);
         }
-        return listViewItems;
+        return stores;
     }
 
 
