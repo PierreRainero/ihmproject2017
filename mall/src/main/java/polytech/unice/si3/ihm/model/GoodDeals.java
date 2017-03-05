c387365e43483147a1b8dca7e80530017ac5b764
Ken Roulamellah <ken.roulamellah@etu.unice.fr>
2017-03-05 01:16:34 +0100

    Ajout des dépendances avec le package store

--- mall/src/main/java/polytech/unice/si3/ihm/model/GoodDeals.java
+++ mall/src/main/java/polytech/unice/si3/ihm/model/GoodDeals.java
@@ -1,8 +1,14 @@
 package polytech.unice.si3.ihm.model;
 
+import polytech.unice.si3.ihm.shop.*;
 /**
  * Created by Ken on 04/03/2017.
  */
 public class GoodDeals {
     private String imagePath;
+    private JsonParser json;
+
+    public GoodDeals(String imagePath){
+        this.imagePath = imagePath;
+    }
 }
