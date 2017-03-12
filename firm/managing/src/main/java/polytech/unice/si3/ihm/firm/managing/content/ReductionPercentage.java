1fc91d25714f31e0724388542374ec82d95e7ba1
Simon Serrano <simon.serrano@etu.unice.fr>
2017-03-13 00:24:21 +0100

    Version alpha de la tab ajout d'un produit

--- firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/content/ReductionPercentage.java
+++ firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/content/ReductionPercentage.java
@@ -1,50 +1,27 @@
 package polytech.unice.si3.ihm.firm.managing.content;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class ReductionPercentage {
 
 
-    private int margin;
-    private List<Integer> percentages;
 
 
-    /**
-     * default constructor for the margin set to 10
-     */
-    public ReductionPercentage() {
-
-        margin = 10;
-        percentages = calculatePercentages();
-    }
 
     /**
      * Constructor for the reduction percentages available for the manager
-     * @param margin custom margin
      */
-    public ReductionPercentage(int margin) {
-        this.margin = margin;
-        percentages = calculatePercentages();
+    public ReductionPercentage() {
+
     }
 
     /**
-     * Method that calculate the percentages depending on the margin
-     * @return the list of percentages
+     * Method that calculate the price depending on the percentage of the reduction
+     * @return the new price
      */
-    public List<Integer> calculatePercentages(){
-        List<Integer> percentages = new ArrayList<>();
-        for (Integer i=0; i<=100; i+=margin){
-            percentages.add(i);
-        }
-        return percentages;
+    public int calculatePrice(int price, int percentage){
+        return price - (price*percentage)/100;
     }
 
-    /**
-     * Getter for the list of percentages setting
-     * @return the list of percentages
-     */
-    public List<Integer> getPercentages() {
-        return percentages;
-    }
+
 }
--- firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/controller/AddProductController.java
+++ firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/controller/AddProductController.java
@@ -1,10 +1,16 @@
 package polytech.unice.si3.ihm.firm.managing.controller;
 
 
-import javafx.event.ActionEvent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import polytech.unice.si3.ihm.firm.managing.content.ReductionPercentage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class AddProductController {
@@ -19,6 +25,7 @@ public class AddProductController {
 
     @FXML
     private TextField productPriceTextField;
+
 
     @FXML
     private TextField productReduction;
@@ -54,6 +61,12 @@ public class AddProductController {
     @FXML
     private Label productImageSelected;
 
+
+
+    private ReductionPercentage reduction = new ReductionPercentage();
+
+    private List<TextField> mustBeCompleted;
+
     @FXML
     /**
      * {@inheritDoc}
@@ -62,6 +75,14 @@ public class AddProductController {
 
         initializeErrors();
         initializeRadioButton();
+        initializeTextField();
+
+        productPromoteNoButton.setSelected(true);
+
+        mustBeCompleted = new ArrayList<>();
+        mustBeCompleted.add(productNameTextField);
+        mustBeCompleted.add(productPriceTextField);
+        mustBeCompleted.add(productReferenceTextField);
 
 
 
@@ -70,7 +91,7 @@ public class AddProductController {
     /**
      * Method that make the errors disappear for the first launch
      */
-    public void initializeErrors(){
+    private void initializeErrors(){
         productPromoteChoiceMissing.setVisible(false);
         productNameMissing.setVisible(false);
         productPriceMissing.setVisible(false);
@@ -81,10 +102,55 @@ public class AddProductController {
 
 
 
-    public void initializeRadioButton(){
+    private void initializeRadioButton(){
         ToggleGroup group = new ToggleGroup();
         productPromoteNoButton.setToggleGroup(group);
         productPromoteYesButton.setToggleGroup(group);
+    }
+
+    private void initializeTextField(){
+        productReduction.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.matches("\\d*")) {
+                    productReduction.setText(newValue.replaceAll("[^\\d]", ""));
+                }
+            }
+        });
+        productNameTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.isEmpty()){
+                    productNameMissing.setVisible(false);
+                }
+                else {
+                    productNameMissing.setVisible(true);
+                }
+            }
+        });
+        productPriceTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.isEmpty()){
+                    productPriceMissing.setVisible(false);
+                }
+                else {
+                    productPriceMissing.setVisible(true);
+                }
+            }
+        });
+
+        productReferenceTextField.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if (!newValue.isEmpty()){
+                    productReferenceMissing.setVisible(false);
+                }
+                else{
+                    productReferenceMissing.setVisible(true);
+                }
+            }
+        });
     }
 
 
@@ -95,9 +161,67 @@ public class AddProductController {
 
 
     @FXML
-    void calculatePrice(ActionEvent event) {
+    void calculatePrice(KeyEvent event) {
+
+        newPriceAfterReduction.setVisible(true);
+        if (productPriceTextField.getText().trim().isEmpty()) {
+            productPriceMissing.setVisible(true);
+
+        }
+        else if (productReduction.getText().trim().equals("")) {
+            newPriceAfterReduction.setText("Nouveau prix : " + productPriceTextField.getText());
+        }
+        else {
+            int price = Integer.parseInt(productPriceTextField.getText());
+            int percentage = Integer.parseInt(productReduction.getText());
+            newPriceAfterReduction.setText("Nouveau prix : " + reduction.calculatePrice(price, percentage));
+
+        }
 
     }
+
+    @FXML
+    void validateFormular(MouseEvent event) {
+
+        if (allValidated()){
+            System.out.println("YESS");
+        }
+
+
+    }
+
+    /**
+     * Checks if all the obligatory text fields are not empty to create the product
+     * @return true if any of them is empty
+     */
+    private boolean allValidated(){
+        int size = 3;
+        if (mustBeCompleted.get(0).getText().trim().equals("")||mustBeCompleted.get(0).getText().trim().isEmpty()){
+            productNameMissing.setVisible(true);
+            size--;
+        }
+        else {
+            productNameMissing.setVisible(false);
+        }
+        if (mustBeCompleted.get(1).getText().trim().isEmpty() || mustBeCompleted.get(1).getText().trim().equals("")){
+            productPriceMissing.setVisible(true);
+            size--;
+        }
+        else{
+            productPriceMissing.setVisible(false);
+        }
+        if (mustBeCompleted.get(2).getText().trim().isEmpty() || mustBeCompleted.get(2).getText().trim().equals("")){
+            productReferenceMissing.setVisible(true);
+            size--;
+        }
+        else{
+            productReferenceMissing.setVisible(false);
+        }
+        if (size==3) return true;
+        return false;
+    }
+
+
 
 
 }
