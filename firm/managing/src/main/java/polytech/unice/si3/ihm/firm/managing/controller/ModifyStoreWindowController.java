4b01e95e5ef8c677495222bab9546ba7a56d9365
Simon Serrano <simon.serrano@etu.unice.fr>
2017-03-13 22:57:26 +0100

    Ajout de la vue de la fenêtre de modification du magasin avec son controller

--- firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/controller/ModifyStoreWindowController.java
+++ firm/managing/src/main/java/polytech/unice/si3/ihm/firm/managing/controller/ModifyStoreWindowController.java
@@ -3,9 +3,26 @@ package polytech.unice.si3.ihm.firm.managing.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
-public class ModifyProductWindowController {
+public class ModifyStoreWindowController {
+
+    @FXML
+    private TextField storeName;
+
+    @FXML
+    private Label storeNameMissing;
+
+    @FXML
+    private TextArea storeDescription;
+
+    @FXML
+    private TextField imagePath;
+
+    @FXML
+    private Button searchComputerButton;
 
     @FXML
     private Button resetButton;
@@ -15,13 +32,6 @@ public class ModifyProductWindowController {
 
     @FXML
     private Button saveButton;
-
-
-    @FXML
-    private Label productNameMissing;
-
-    @FXML
-    private Label productPriceMissing;
 
     @FXML
     void cancelTheWindow(MouseEvent event) {
@@ -37,10 +47,10 @@ public class ModifyProductWindowController {
     void saveModifications(MouseEvent event) {
 
     }
+
     @FXML
     void searchComputer(MouseEvent event) {
 
     }
 
 }
-
